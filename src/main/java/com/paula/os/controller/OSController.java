e308a2482c313c82f10d763557dcd85faa79119e
Paula <paulasantos_risa@hotmail.com>
2023-01-24 14:47:58 +0000

    system properties file

--- src/main/java/com/paula/os/controller/OSController.java
+++ src/main/java/com/paula/os/controller/OSController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.paula.os.dtos.OSDTO;
 import com.paula.os.services.OSService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/os")
 public class OSController {
--- src/main/java/com/paula/os/controller/TechnicController.java
+++ src/main/java/com/paula/os/controller/TechnicController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.paula.os.domain.Technic;
 import com.paula.os.dtos.TechnicDTO;
 import com.paula.os.services.TechnicService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value= "/technics")
 public class TechnicController {
